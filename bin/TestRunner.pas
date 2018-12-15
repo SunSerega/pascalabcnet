0bce524086fdca4453a82a658f35925a4690479b
Sun Serega <latchenko3@yandex.ru>
2018-12-15 14:54:14 +0200

    Выглядит стабильно

--- bin/TestRunner.pas
+++ bin/TestRunner.pas
@@ -10,71 +10,53 @@
 uses PascalABCCompiler, System.IO, System.Diagnostics;
 
 var
-  TestSuiteDir: string;
-
-var
-  PathSeparator: string := Path.DirectorySeparatorChar;
-
-function IsUnix: boolean;
-begin
-  Result := (System.Environment.OSVersion.Platform = System.PlatformID.Unix) or (System.Environment.OSVersion.Platform = System.PlatformID.MacOSX);  
-end;
-
-function GetTestSuiteDir: string;
-begin
-  var dir := Path.GetDirectoryName(GetEXEFileName());
-  var ind := dir.LastIndexOf('bin');
-  Result := dir.Substring(0, ind) + 'TestSuite';
-end;
-
-function GetLibDir: string;
-begin
-  var dir := Path.GetDirectoryName(GetEXEFileName());
-  Result := dir + PathSeparator + 'Lib';
-end;
-
-procedure CompileErrorTests(withide: boolean);
-begin
+  PathSep := Path.DirectorySeparatorChar;
+  IsNotWin := (System.Environment.OSVersion.Platform = System.PlatformID.Unix) or (System.Environment.OSVersion.Platform = System.PlatformID.MacOSX);
   
-  var comp := new Compiler();
-  
-  var files := Directory.GetFiles(TestSuiteDir + PathSeparator + 'errors', '*.pas');
-  for var i := 0 to files.Length - 1 do
-  begin
-    var content := &File.ReadAllText(files[i]);
-    if content.StartsWith('//winonly') and IsUnix then
-      continue;
-    var co: CompilerOptions := new CompilerOptions(files[i], CompilerOptions.OutputType.ConsoleApplicaton);
-    co.Debug := true;
-    co.OutputDirectory := TestSuiteDir + PathSeparator + 'errors';
-    co.UseDllForSystemUnits := false;
-    co.RunWithEnvironment := withide;
-    comp.ErrorsList.Clear();
-    comp.Warnings.Clear();
-    
-    comp.Compile(co);
-    if comp.ErrorsList.Count = 0 then
-    begin
-      System.Windows.Forms.MessageBox.Show('Compilation of error sample ' + files[i] + ' was successfull' + System.Environment.NewLine);
-      Halt();
-    end
-    else if comp.ErrorsList.Count = 1 then
-    begin
-      if comp.ErrorsList[0].GetType() = typeof(PascalABCCompiler.Errors.CompilerInternalError) then
-        System.Windows.Forms.MessageBox.Show('Compilation of ' + files[i] + ' failed' + System.Environment.NewLine + comp.ErrorsList[0].ToString());
-    end;
-    if i mod 20 = 0 then
-      System.GC.Collect();
-  end;
-  
-end;
+  TestSuiteDir := Concat(Path.GetDirectoryName(GetCurrentDir), PathSep, 'TestSuite');
+  LibDir := Concat(GetCurrentDir, PathSep, 'Lib');
+
+{$region Compiling}
 
 type
   [System.Serializable]
-  CompileAllRunTests_Helper = auto class
+  ///Предоставляет метод, компилирующий несколько файлов,
+  ///И который можно кидать между доменами
+  BatchCompHelper = class
     
-    withdll, only32bit: boolean;
+    otp_dir: string;
+    with_dll, only32bit, with_ide, expect_error: boolean;
     batch: array of string;
+    
+    ///Обычная компиляция
+    static function GetStdMethod(batch: array of string; with_dll: boolean; only32bit: boolean := false): System.CrossAppDomainDelegate;
+    begin
+      var bch := new BatchCompHelper;
+      bch.batch := batch;
+      bch.otp_dir := Concat(TestSuiteDir, PathSep, 'exe');
+      
+      bch.with_dll := with_dll;
+      bch.only32bit := only32bit;
+      bch.with_ide := false;
+      bch.expect_error := false;
+      
+      Result := bch.Exec;
+    end;
+    
+    ///Компиляция, ожидающая ошибку
+    static function GetErrMethod(batch: array of string; with_ide: boolean): System.CrossAppDomainDelegate;
+    begin
+      var bch := new BatchCompHelper;
+      bch.batch := batch;
+      bch.otp_dir := Concat(TestSuiteDir, PathSep, 'exe');
+      
+      bch.with_dll := true;
+      bch.only32bit := true;
+      bch.with_ide := with_ide;
+      bch.expect_error := true;
+      
+      Result := bch.Exec;
+    end;
     
     procedure Exec;
     begin
@@ -82,159 +64,116 @@ type
       
       foreach var fname in batch do
       begin
-        var content := &File.ReadAllText(fname);
-        if content.StartsWith('//winonly') and IsUnix then
-          continue;
+        if &File.ReadAllText(fname).Contains('//winonly') and IsNotWin then continue;
+        
+        
+        
         var co: CompilerOptions := new CompilerOptions(fname, CompilerOptions.OutputType.ConsoleApplicaton);
         co.Debug := true;
-        co.OutputDirectory := TestSuiteDir + PathSeparator + 'exe';
-        co.UseDllForSystemUnits := withdll;
-        co.RunWithEnvironment := false;
+        co.OutputDirectory := otp_dir;
+        
+        co.UseDllForSystemUnits := with_dll;
+        co.RunWithEnvironment := with_ide;
         co.IgnoreRtlErrors := false;
         co.Only32Bit := only32bit;
+        
+        
+        
+        comp.Compile(co);
+        
+        if expect_error then
+        begin
+          
+          if comp.ErrorsList.Count = 0 then
+          begin
+            System.Console.WriteLine($'Compilation of error sample {fname} was successfull');
+            Halt(-1);
+          end else
+          foreach var err in comp.ErrorsList do
+            //ToDo найти почему без "as object" не работает
+            if err as object is Errors.CompilerInternalError then
+            begin
+              System.Console.WriteLine($'Compilation of {fname} failed with internal error{System.Environment.NewLine}{err}');
+              Halt(-1);
+            end;
+          
+        end else
+        if comp.ErrorsList.Count <> 0 then
+        begin
+          
+          System.Console.WriteLine($'Compilation of {fname} failed{System.Environment.NewLine}{comp.ErrorsList[0]}');
+          Halt(-1);
+          
+        end;
+        
+        
+        
         comp.ErrorsList.Clear();
         comp.Warnings.Clear();
         
-        comp.Compile(co);
-        if comp.ErrorsList.Count > 0 then
-        begin
-          System.Windows.Forms.MessageBox.Show($'Compilation of {fname} failed{System.Environment.NewLine}{comp.ErrorsList[0]}');
-          Halt();
-        end;
       end;
       
     end;
     
   end;
 
-procedure CompileAllRunTests(withdll: boolean; only32bit: boolean := false);
+procedure CompileAll(path: string; with_dll: boolean; only32bit: boolean := false);
 begin
   
-  foreach var batch in Directory.GetFiles(TestSuiteDir, '*.pas').Take(50).Batch(30) do
+  foreach var _batch in Directory.EnumerateFiles(path, '*.pas').Batch(30) do
   begin
-    var ad := System.AppDomain.CreateDomain('TestRunner sub domain');
+    var batch := _batch.ToArray;
+    //Надо обязательно выполнять в отдельном домене
+    //Иначе не выйдет удалить сборки, которые создаёт компилятор
+    var ad := System.AppDomain.CreateDomain('TestRunner sub domain for compiling');
     
     ad.DoCallBack(
-      CompileAllRunTests_Helper.Create(
-        withdll, only32bit,
-        batch.ToArray
-      ).Exec
+      BatchCompHelper.GetStdMethod(
+        batch,
+        with_dll, only32bit
+      )
+    );
+    
+    //Эта строчка удаляет все полученные компилятором сборки
+    System.AppDomain.Unload(ad);
+  end;
+  
+end;
+
+procedure CompileAllErr(path: string; with_ide: boolean);
+begin
+  
+  foreach var _batch in Directory.EnumerateFiles(path, '*.pas').Batch(30) do
+  begin
+    var batch := _batch.ToArray;
+    var ad := System.AppDomain.CreateDomain('TestRunner sub domain for compiling');
+    
+    ad.DoCallBack(
+      BatchCompHelper.GetErrMethod(
+        batch,
+        with_ide
+      )
     );
     
     System.AppDomain.Unload(ad);
-    System.GC.Collect();
-  end;
-  
-  //Println;
-end;
-
-procedure CompileAllCompilationTests(dir: string; withdll: boolean);
-begin
-  
-  var comp := new Compiler();
-  
-  var files := Directory.GetFiles(TestSuiteDir + PathSeparator + dir, '*.pas');
-  for var i := 0 to files.Length - 1 do
-  begin
-    var content := &File.ReadAllText(files[i]);
-    if content.StartsWith('//winonly') and IsUnix then
-      continue;
-    var co: CompilerOptions := new CompilerOptions(files[i], CompilerOptions.OutputType.ConsoleApplicaton);
-    co.Debug := true;
-    co.OutputDirectory := TestSuiteDir + PathSeparator + dir;
-    co.UseDllForSystemUnits := withdll;
-    co.RunWithEnvironment := false;
-    co.IgnoreRtlErrors := false;
-    comp.ErrorsList.Clear();
-    comp.Warnings.Clear();
-    
-    comp.Compile(co);
-    if comp.ErrorsList.Count > 0 then
-    begin
-      System.Windows.Forms.MessageBox.Show('Compilation of ' + files[i] + ' failed' + System.Environment.NewLine + comp.ErrorsList[0].ToString());
-      Halt();
-    end;
-    if i mod 20 = 0 then
-      System.GC.Collect();
   end;
   
 end;
 
-procedure CompileAllUnits;
-begin
-  var comp := new Compiler();
-  var files := Directory.GetFiles(TestSuiteDir + PathSeparator + 'units', '*.pas');
-  var dir := TestSuiteDir + PathSeparator + 'units' + PathSeparator;
-  for var i := 0 to files.Length - 1 do
-  begin
-    var content := &File.ReadAllText(files[i]);
-    if content.StartsWith('//winonly') and IsUnix then
-      continue;
-    var co: CompilerOptions := new CompilerOptions(files[i], CompilerOptions.OutputType.ConsoleApplicaton);
-    co.Debug := true;
-    co.OutputDirectory := dir;
-    co.UseDllForSystemUnits := false;
-    comp.ErrorsList.Clear();
-    comp.Warnings.Clear();
-    
-    comp.Compile(co);
-    if comp.ErrorsList.Count > 0 then
-    begin
-      System.Windows.Forms.MessageBox.Show('Compilation of ' + files[i] + ' failed' + System.Environment.NewLine + comp.ErrorsList[0].ToString());
-      Halt();
-    end;
-  end;
-  System.GC.Collect;
-end;
+{$endregion Compiling}
 
-procedure CompileAllUsesUnits;
-begin
-  System.Environment.CurrentDirectory := Path.GetDirectoryName(GetEXEFileName());
-  var comp := new Compiler();
-  
-  var files := Directory.GetFiles(TestSuiteDir + PathSeparator + 'usesunits', '*.pas');
-  for var i := 0 to files.Length - 1 do
-  begin
-    var content := &File.ReadAllText(files[i]);
-    if content.StartsWith('//winonly') and IsUnix then
-      continue;
-    var co: CompilerOptions := new CompilerOptions(files[i], CompilerOptions.OutputType.ConsoleApplicaton);
-    co.Debug := true;
-    co.OutputDirectory := TestSuiteDir + PathSeparator + 'exe';
-    co.UseDllForSystemUnits := false;
-    comp.ErrorsList.Clear();
-    comp.Warnings.Clear();
-    comp.Compile(co);
-    if comp.ErrorsList.Count > 0 then
-    begin
-      System.Windows.Forms.MessageBox.Show('Compilation of ' + files[i] + ' failed' + System.Environment.NewLine + comp.ErrorsList[0].ToString());
-      Halt();
-    end;
-  end;
-  System.GC.Collect;
-end;
-
-procedure CopyPCUFiles;
-begin
-  System.Environment.CurrentDirectory := Path.GetDirectoryName(GetEXEFileName());
-  var files := Directory.GetFiles(TestSuiteDir + PathSeparator + 'units', '*.pcu');
-  
-  foreach fname: string in files do
-  begin
-    &File.Move(fname, TestSuiteDir + PathSeparator + 'usesunits' + PathSeparator + Path.GetFileName(fname));
-  end;
-end;
+{$region Runing}
 
 procedure RunAllTests(redirectIO: boolean);
 begin
-  var files := Directory.GetFiles(TestSuiteDir + PathSeparator + 'exe', '*.exe');
+  var files := Directory.GetFiles(TestSuiteDir + PathSep + 'exe', '*.exe');
   for var i := 0 to files.Length - 1 do
   begin
     var psi := new System.Diagnostics.ProcessStartInfo(files[i]);
     psi.CreateNoWindow := true;
     psi.UseShellExecute := false;
     
-    psi.WorkingDirectory := TestSuiteDir + PathSeparator + 'exe';
+    psi.WorkingDirectory := TestSuiteDir + PathSep + 'exe';
 		  {psi.RedirectStandardInput := true;
 		  psi.RedirectStandardOutput := true;
 		  psi.RedirectStandardError := true;}
@@ -261,50 +200,35 @@ begin
   CodeCompletion.CodeCompletionTester.Test();  
 end;
 
-function GetLineByPos(lines: array of string; pos: integer): integer;
-begin
-  var cum_pos := 0;
-  for var i := 0 to lines.Length - 1 do
-    for var j := 0 to lines[j].Length - 1 do
-    begin
-      if cum_pos = pos then
-      begin
-        Result := i + 1;
-        exit;
-      end;
-      Inc(cum_pos);  
-    end;
-end;
-
-function GetColByPos(lines: array of string; pos: integer): integer;
-begin
-  var cum_pos := 0;
-  for var i := 0 to lines.Length - 1 do
-    for var j := 0 to lines[j].Length - 1 do
-    begin
-      if cum_pos = pos then
-      begin
-        Result := j + 1;
-        exit;
-      end;
-      Inc(cum_pos);  
-    end;
-end;
-
 procedure RunIntellisenseTests;
 begin
   PascalABCCompiler.StringResourcesLanguage.CurrentTwoLetterISO := 'ru';
-  CodeCompletion.CodeCompletionTester.TestIntellisense(TestSuiteDir + PathSeparator + 'intellisense_tests');
+  CodeCompletion.CodeCompletionTester.TestIntellisense(TestSuiteDir + PathSep + 'intellisense_tests');
 end;
 
 procedure RunFormatterTests;
 begin
   CodeCompletion.FormatterTester.Test();
-  var errors := &File.ReadAllText(TestSuiteDir + PathSeparator + 'formatter_tests' + PathSeparator + 'output' + PathSeparator + 'log.txt');
+  var errors := &File.ReadAllText(TestSuiteDir + PathSep + 'formatter_tests' + PathSep + 'output' + PathSep + 'log.txt');
   if not string.IsNullOrEmpty(errors) then
   begin
     System.Windows.Forms.MessageBox.Show(errors + System.Environment.NewLine + 'more info at TestSuite/formatter_tests/output/log.txt');
     Halt;
+  end;
+end;
+
+{$endregion Runing}
+
+{$region FileMoving}
+
+procedure CopyPCUFiles;
+begin
+  System.Environment.CurrentDirectory := Path.GetDirectoryName(GetEXEFileName());
+  var files := Directory.GetFiles(TestSuiteDir + PathSep + 'units', '*.pcu');
+  
+  foreach fname: string in files do
+  begin
+    &File.Move(fname, TestSuiteDir + PathSep + 'usesunits' + PathSep + Path.GetFileName(fname));
   end;
 end;
 
@@ -323,69 +247,75 @@ end;
 
 procedure ClearExeDir;
 begin
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'exe', '*.*');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'CompilationSamples', '*.exe');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'CompilationSamples', '*.mdb');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'CompilationSamples', '*.pdb');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'CompilationSamples', '*.pcu');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'pabcrtl_tests', '*.exe');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'pabcrtl_tests', '*.pdb');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'pabcrtl_tests', '*.mdb');
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'pabcrtl_tests', '*.pcu');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'exe', '*.*');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'CompilationSamples', '*.exe');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'CompilationSamples', '*.mdb');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'CompilationSamples', '*.pdb');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'CompilationSamples', '*.pcu');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'pabcrtl_tests', '*.exe');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'pabcrtl_tests', '*.pdb');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'pabcrtl_tests', '*.mdb');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'pabcrtl_tests', '*.pcu');
 end;
 
 procedure DeletePCUFiles;
 begin
-  ClearDirByPattern(TestSuiteDir + PathSeparator + 'usesunits', '*.pcu');
-end;
-
-procedure DeletePABCSystemPCU;
-begin
-  var dir := Path.Combine(Path.GetDirectoryName(GetEXEFileName()), 'Lib');
-  var pcu := Path.Combine(dir, 'PABCSystem.pcu');
+  ClearDirByPattern(TestSuiteDir + PathSep + 'usesunits', '*.pcu');
 end;
 
 procedure CopyLibFiles;
 begin
-  var files := Directory.GetFiles(GetLibDir(), '*.pas');
+  var files := Directory.GetFiles(LibDir, '*.pas');
   foreach f: string in files do
   begin
-    &File.Copy(f, TestSuiteDir + PathSeparator + 'CompilationSamples' + PathSeparator + Path.GetFileName(f), true);
+    &File.Copy(f, TestSuiteDir + PathSep + 'CompilationSamples' + PathSep + Path.GetFileName(f), true);
   end;
 end;
 
+{$endregion FileMoving}
+
+{$region Misc}
+
+function TestCLParam(par: string): boolean :=
+(ParamCount = 0) or (ParamStr(1) = par);
+
+function TSSF(dir: string) :=
+Concat(TestSuiteDir, PathSep, dir);
+
+{$endregion Misc}
+
 begin
-  //DeletePABCSystemPCU;
   try
-    TestSuiteDir := GetTestSuiteDir;
-    System.Environment.CurrentDirectory := Path.GetDirectoryName(GetEXEFileName());
     
-    if (ParamCount = 0) or (ParamStr(1) = '1') then
+    //readln;
+    System.Environment.CurrentDirectory := TestSuiteDir;
+    
+    if TestCLParam('1') then
     begin
       DeletePCUFiles;
       ClearExeDir;
-      CompileAllRunTests(false);
+      CompileAll(TestSuiteDir, false);
+      Writeln('Done compiling RunTests (main dir)');
     end;
     
-    System.GC.Collect;
-    ReadlnString('CompileAllRunTests done');
     
-    if (ParamCount = 0) or (ParamStr(1) = '2') then
+    if TestCLParam('2') then
     begin
       CopyLibFiles;
-      CompileAllCompilationTests('CompilationSamples', false);
+      CompileAll(TSSF('CompilationSamples'), false);
+      Writeln('Done compiling CompTests (CompilationSamples dir)');
     end;
     
-    if (ParamCount = 0) or (ParamStr(1) = '3') then
+    if TestCLParam('3') then
     begin
-      CompileAllUnits;
+      CompileAll(TSSF('units'), false);
       CopyPCUFiles;
-      CompileAllUsesUnits;
-      CompileErrorTests(false);
-      writeln('Tests compiled successfully');
+      CompileAll(TSSF('usesunits'), false);
+      CompileAllErr(TSSF('errors'), false);
+      writeln('Done with units and ErrTests');
     end;
     
-    if (ParamCount = 0) or (ParamStr(1) = '4') then
+    if TestCLParam('4') then
     begin
       RunAllTests(false);
       writeln('Tests run successfully');
@@ -393,29 +323,43 @@ begin
       DeletePCUFiles;
     end;
     
-    if (ParamCount = 0) or (ParamStr(1) = '5') then
+    if TestCLParam('5') then
     begin
-      CompileAllRunTests(true);
+      
+      CompileAll(TestSuiteDir, true);
       writeln('Tests with pabcrtl compiled successfully');
-      CompileAllCompilationTests('pabcrtl_tests', true);
+      
+      CompileAll(TSSF('pabcrtl_tests'), true);
       RunAllTests(false);
       writeln('Tests with pabcrtl run successfully');
       ClearExeDir;
-      CompileAllRunTests(false, true);
+      
+      CompileAll(TestSuiteDir, false,true);
       writeln('Tests in 32bit mode compiled successfully');
       RunAllTests(false);
       writeln('Tests in 32bit run successfully');
-      System.Environment.CurrentDirectory := Path.GetDirectoryName(GetEXEFileName());
+      
+      System.Environment.CurrentDirectory := Path.GetDirectoryName(GetEXEFileName);
       RunExpressionsExtractTests;
       writeln('Intellisense expression tests run successfully');
+      
       RunIntellisenseTests;
       writeln('Intellisense tests run successfully');
+      
       RunFormatterTests;
       writeln('Formatter tests run successfully');
+      
     end;
+    
+    if not System.Console.IsOutputRedirected then Readln;
     
   except
     on e: Exception do
-      assert(false, e.ToString());
+    begin
+      Writeln('Exception:');
+      Writeln(e);
+      if not System.Console.IsOutputRedirected then Readln;
+      Halt(-1);
+    end;
   end;
 end.
