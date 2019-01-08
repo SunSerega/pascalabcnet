dcd48d47b7e64018da802202e5b91b7e17204683
Sun Serega <latchenko3@yandex.ru>
2019-01-08 10:28:51 +0200

    -

---
+++ Configuration/GlobalAssemblyInfo.cs
@@ -0,0 +1,23 @@
+//Generated from GlobalAssemblyInfo.cs.tmpl && Version
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PascalABC.NET Compiler")]
+[assembly: AssemblyCopyright("Copyright © 2005-2018 by Ivan Bondarev, Stanislav Mihalkovich")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+[assembly: AssemblyVersion(RevisionClass.FullVersion)]
+
+internal static class RevisionClass
+{
+	public const string Major = "3";
+	public const string Minor = "4";
+	public const string Build = "2";
+	public const string Revision = "1931";
+
+        public const string MainVersion = Major + "." + Minor;
+	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision;
+}
+	
