161de4b3965db968af0c72eff05794e663fe3f12
Sun Serega <latchenko3@yandex.ru>
2021-02-20 20:29:14 +0200

    added .bat for custom build to git

---
+++ Update custom build.bat
@@ -0,0 +1,26 @@
+﻿
+
+
+git.exe checkout -f master --
+@IF %ERRORLEVEL% NEQ 0 GOTO END
+
+git.exe pull --progress -v --no-rebase "git@github.com:pascalabcnet/pascalabcnet.git" master
+@IF %ERRORLEVEL% NEQ 0 GOTO END
+
+git.exe push --progress "origin" master:master
+@IF %ERRORLEVEL% NEQ 0 GOTO END
+
+git.exe checkout custom-build --
+@IF %ERRORLEVEL% NEQ 0 GOTO END
+
+git.exe merge master
+@IF %ERRORLEVEL% NEQ 0 GOTO END
+
+git.exe push --progress "origin" custom-build:custom-build
+@IF %ERRORLEVEL% NEQ 0 GOTO END
+
+
+
+_GenerateAllSetups.bat
+:END
+PAUSE
